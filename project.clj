45a7779bfa6839889dabfbbf50faab84ac909091
Chris O'Donnell <chris.bowdoin@gmail.com>
2016-10-25 18:10:36 -0400

    Push version to 0.2.0-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tornyxorn "0.1.0-SNAPSHOT"
+(defproject tornyxorn "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
